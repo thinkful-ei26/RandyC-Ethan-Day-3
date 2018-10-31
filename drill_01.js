a00c1a956a69d61b41dc3a53d7c550ff1f39988b
R-Cole <randy@goodglue.com>
2018-10-31 09:08:12 -0700

    adding drill 1

---
+++ drill_01.js
@@ -0,0 +1,17 @@
+// Object creator
+// In this drill, you need to write code for the createMyObject function so that it returns an object with the following key value pairs:
+
+// foo => 'bar'
+// answerToUniverse => 42
+// olly olly => 'oxen free'
+// sayHello => a function that returns the string 'hello'
+
+
+function createMyObject(){
+
+    return { foo: 'bar', answerToUniverse: '42','olly olly': 'oxen free',sayHello: function(){return 'hello'}}
+
+ 
+}
+
+console.log(createMyObject());
