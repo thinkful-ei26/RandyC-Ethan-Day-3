53d3bb3fe9c6cf29fb754b387c24e3f8f31398b9
Ethan Frigon <ethan.frigon@gmail.com>
2018-10-31 09:17:16 -0700

    drill 2

---
+++ drill_02.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Modify the updateObject function (which takes a single argument (obj)) so that it adds the following key/value pairs to obj and returns it:
+
+// foo => 'foo'
+// bar => 'bar'
+// bizz => 'bizz'
+// bang => 'bang'
+
+
+function updateObject(obj) {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj['bang'] = 'bang';
+  return obj;
+}
+
+console.log(updateObject({}));
