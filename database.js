3e2be1f1899541ce9ecd30a7749c6c7e0f676e67
Ethan Frigon <ethan.frigon@gmail.com>
2018-10-31 17:07:37 -0700

    a database solution!

--- database.js
+++ database.js
@@ -14,18 +14,35 @@ const HEROES = [
 
 function findOne (arr, query) {
   //console.log(query.map(obj => Object.values(obj)));
-  let queryTransform = Object.values(query).join(',');
+  let queryTransform = Object.values(query);
   //let queryArray = query.map(obj => Object.values(obj));
   let valueArray = arr.map(obj => Object.values(obj));
   //return arr.find(obj => obj.name === query);
   //return arr;
-  if (queryTransform === toString(valueArray[0][0])){
-    return arr[0];
+  // if (queryTransform === toString(valueArray[0][0])){
+  //   return arr[0];
+  // }
+  // console.log(valueArray[0][0]);
+  for (let i = 0; i < valueArray.length; i++){
+    if (((valueArray[i][0] === queryTransform[0]) && 
+      (!queryTransform[1] || valueArray[i][1] === queryTransform[1] || valueArray[i][2] === queryTransform[1]) && 
+      (!queryTransform[2] || valueArray[i][2] === queryTransform[2])) || 
+
+      (queryTransform[0] === valueArray[i][1] && 
+      (!queryTransform[1] ||  valueArray[i][2] === queryTransform[1])) ||
+
+      (queryTransform[0] === valueArray[i][2])) {
+      return arr[i];
+    }
   }
-  console.log(valueArray[0][0]);
   console.log(valueArray);
   console.log(queryTransform);
 }
 
 
-console.log(findOne(HEROES, {id: 1}));
+console.log(findOne(HEROES, {squad: 'Justice League'}));
+
+console.log(findOne(HEROES, { id: 5, squad: 'Justice League' }));
+
+console.log(findOne(HEROES, { id: 4, name: 'Superman', squad: 'Justice League' }));
+
